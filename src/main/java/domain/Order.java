46aa02ec1ca1b511bf7695bc9c923affa8a991b3
JordyFrijters <jcjmfrij@avans.nl>
2020-02-01 18:13:43 +0100

    Probeersel
    Proberen alles werkend te krijgen. Het wijzigen van de prijzen krijg ze niet opgehaald.

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 
 public class Order
@@ -27,9 +29,64 @@ public class Order
         tickets.add(ticket);
     }
 
-    public double calculatePrice()
+    public void calculatePrice()
     {
-        return 0;
+       MovieTicket selectedTicket = this.tickets.get(0);
+       double PricePerSeatGet = selectedTicket.getPrice();
+       boolean CheckPremium = selectedTicket.isPremiumTicket();
+       boolean CheckStudentOrder = this.isStudentOrder;
+
+
+       //2e Kaartje gratis
+        if(tickets.size() >= 2){
+            //get second ticket
+            MovieTicket secondticket = tickets.get(1);
+
+            //Ik kreeg niet de tijd van de screening te pakken daarom de tijd gepakt van bestellen. WIJZIG DIT
+            String DayOfOrder = LocalDate.now().getDayOfWeek().toString();
+
+            //student
+            if(CheckStudentOrder == true){
+                double newprice = 0.00;
+                //pak prijs van ticket en wijzig prijs naar 0
+
+            }
+            //geen student
+            else{
+                if(DayOfOrder != "FRIDAY" ||DayOfOrder != "SATURDAY" ||DayOfOrder != "SUNDAY" ){
+                    double newprice = 0.00;
+                    //pak prijs van ticket en wijzig prijs naar 0;
+
+                }else{
+                    if(tickets.size() >= 6){
+                        double TotaalAmount = 0.00;
+                        for (int i = 1; i <= tickets.size(); i++){
+                            MovieTicket SelectedTicket = tickets.get(i);
+                            double Price = SelectedTicket.getPrice();
+                            TotaalAmount = TotaalAmount + Price;
+                        }
+                        TotaalAmount = TotaalAmount*0.9;
+
+                        //verdeel de prijs over de tickets zodat iedereen een neiuwe amount krijgt
+                    }
+                }
+            }
+
+        }
+
+        //
+
+       //PrijsPremium
+        if(CheckPremium == true && CheckStudentOrder == true){
+            
+            PricePerSeatGet = PricePerSeatGet + 2.00;
+        }
+        if(CheckPremium == true && CheckStudentOrder == false){
+            PricePerSeatGet = PricePerSeatGet + 3.00;
+        }
+
+
+        return ;
     }
 
     public void export(TicketExportFormat exportFormat)
