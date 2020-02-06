93324ae5836c04579a8b8d8051e06f32b5fc6bff
Jim van Zuidam <j.vanzuidam@student.avans.nl>
2020-02-06 10:33:30 +0100

    First simple test

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,2 +1,31 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import domain.Order;
+import org.junit.Test;
+
 public class OrderTest {
+
+    public boolean isPositiveNumber(int number) {
+
+        boolean result = false;
+        if (number >= 1) {
+            result = true;
+        }
+        return result;
+    }
+
+    @Test
+    public void checkIfOrderNrIsPositive() {
+        //Arrange
+        Order order = new Order(0, true);
+        OrderTest orderTest = new OrderTest();
+
+        //Act
+        int result = order.getOrderNr();
+
+
+        //Assert
+        assertTrue(orderTest.isPositiveNumber(result));
+    }
 }
